f853f11c7ca6784a755f3c42f8762407a9693da4
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2017-12-06 14:04:53 -0400

    Remove hiden files

--- utils.js
+++ utils.js
@@ -12,14 +12,3 @@ function normalizePropToReactStyle(property) {
     return txt.slice(1).toUpperCase();
   });
 }
-;
-
-var _temp = function () {
-  if (typeof __REACT_HOT_LOADER__ === 'undefined') {
-    return;
-  }
-
-  __REACT_HOT_LOADER__.register(normalizePropToReactStyle, "normalizePropToReactStyle", "src/utils.js");
-}();
-
-;
